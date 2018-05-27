361eb475495bc3b3715dc0937a36243e0549cbfb
LAPTOP\Fergus <fergusfrl@gmail.com>
2018-05-27 12:36:57 +1200

    enriched data collection to include height and location

--- index.js
+++ index.js
@@ -9,14 +9,9 @@ let port = process.env.PORT || 3030;
 let url =
     "http://data.ecan.govt.nz/data/57/Water/River%20Stage%20flow%20summary%20by%20area/JSON?Sites=NORTH";
 
-// *** PROGRAM PLAN ***
-// Every ~30 minutes - request information from each cousil API w/ axios
-// Store this information in as a json object
-// Send this json object w/ express GET
-
 // setInterval(() => {
 //     console.log("Flows last checked at " + new Date());
-//     // axios here
+//     // TODO: put program here so updates occur every _X_ seconds
 // }, 10000); // 1sec == 1000
 
 // Get counsil API river data
@@ -25,9 +20,16 @@ axios.get(url).then(response => {
         return {
             siteName: site.Site_x0020_Name,
             currentFlow: site.Flow,
-            lastUpdated: site.Last_x0020_Sample
+            currentLevel: site.Stage_x0020_Height,
+            lastUpdated: site.Last_x0020_Sample,
+            coordinates: {
+                lat: site.WGS84_Latitude,
+                lng: site.WGS84_Longitude
+            }
         };
     });
+
+    // TODO: currently used for development - will remove before final deploy
     console.log(siteData);
 
     // Send data w/ express to create API
