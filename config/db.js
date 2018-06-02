24fb02471bcd97a776ddc7224c35974f674fa3de
LAPTOP\Fergus <fergusfrl@gmail.com>
2018-06-03 08:52:38 +1200

    type

--- config/db.js
+++ config/db.js
@@ -2,4 +2,4 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 mongoose
     .connect("mongodb://<dbuser>:<dbpassword>@ds121089.mlab.com:21089/rivers")
-    .catch(err => consoole.log(err));
+    .catch(err => console.log(err));
