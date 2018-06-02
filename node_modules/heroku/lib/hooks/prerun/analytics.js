17e40f530daa67dde9bfcc8e3baf2a91f32c83fb
LAPTOP\Fergus <fergusfrl@gmail.com>
2018-06-02 12:07:01 +1200

    removed node_modules dir

--- node_modules/heroku/lib/hooks/prerun/analytics.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const analytics_1 = require("../../analytics");
-exports.analytics = async function (options) {
-    const analytics = new analytics_1.default(this.config);
-    await analytics.record(options);
-};
