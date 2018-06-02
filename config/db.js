4ae8f9f84483fcb694168c819dbf3216108f8e80
LAPTOP\Fergus <fergusfrl@gmail.com>
2018-06-03 08:53:31 +1200

    fixed mongodb uri

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 mongoose
-    .connect("mongodb://<dbuser>:<dbpassword>@ds121089.mlab.com:21089/rivers")
+    .connect("mongodb://fergusfrl:fergusfrl@ds121089.mlab.com:21089/rivers")
     .catch(err => console.log(err));
