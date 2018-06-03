227ddd67fe1cb1ab53e87c485453fed7bdb225ad
LAPTOP\Fergus <fergusfrl@gmail.com>
2018-06-03 13:16:29 +1200

    removed config/db.js

--- config/db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
-mongoose
-    .connect("mongodb://fergusfrl:fergusfrl@ds121089.mlab.com:21089/rivers")
-    .catch(err => console.log(err));
