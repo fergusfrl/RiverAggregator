0b7bc19e72af64df7287a19bcb8cf416cc8d43d6
LAPTOP\Fergus <fergusfrl@gmail.com>
2018-08-25 13:07:08 +1200

    removed regex from siteName variable

--- index.js
+++ index.js
@@ -150,7 +150,7 @@ function mapData() {
 }
 
 // get current data for an individual site
-app.get("/:siteName(^(?!graphql).*([$]))", (req, res) => {
+app.get("/:siteName", (req, res) => {
     Gauge.findOne({ siteName: req.params.siteName.toLowerCase() })
         .then(data =>
             res.send({
